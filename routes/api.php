73db48a0481565a497599cfd14eccf06e79df35d
akrs-code <abdulkhaliqsolaiman@gmail.com>
2025-09-19 13:42:04 +0800

    feat(routes): added a dynamic routes for condo, amenity, booking

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CondoController;
+use App\Http\Controllers\BookingController;
+use App\Http\Controllers\AmenityController;
+
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -18,10 +22,13 @@ Route::get('/test', function () {
     ]);
 });
 
+//condos
+Route::apiResource('condos', CondoController::class);
 
-Route::get('/condos', [CondoController::class, 'index']);
-Route::get('/condos/{id}', [CondoController::class, 'show']);
-Route::post('/condos', [CondoController::class, 'store']);
-Route::put('/condos/{id}', [CondoController::class, 'update']);
-Route::delete('/condos/{id}', [CondoController::class, 'destroy']);
+//bookings
+Route::apiResource('bookings', BookingController::class);
+
+//amenities
+Route::apiResource('amenities', AmenityController::class);
+
 
